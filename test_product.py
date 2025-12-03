f0d41781a286a0abaa19cdbdf6d219c82c2cc9ab
Utsav-83 <utsav8391@gmail.com>
2025-12-03 23:49:59 +0530

    Add unit test for get_product_info function

---
+++ test_product.py
@@ -0,0 +1,14 @@
+# test_product.py
+from product import get_product_info
+
+def test_get_product_info():
+    result = get_product_info("P101", "Keyboard", 5, 750)
+
+    expected = (
+        "Product ID: P101\n"
+        "Name: Keyboard\n"
+        "Quantity: 5\n"
+        "Price: 750"
+    )
+
+    assert result == expected
